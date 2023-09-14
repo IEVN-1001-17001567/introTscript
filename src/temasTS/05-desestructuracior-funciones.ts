a75ce60405bf300a350dc4dfbcaf40cf8c8eefea
Rodrigo Gomez <rodrigogp4812@gmail.com>
2023-09-13 18:44:21 -0600

    '13/09/2023 RodrigoGomezPadilla'

---
+++ src/temasTS/05-desestructuracior-funciones.ts
@@ -0,0 +1,41 @@
+
+
+export interface IProducto{
+    desc:string;
+    precio:number;
+
+}
+const telefono:IProducto={
+    desc:'Sony erickson',
+    precio:1298
+}
+
+const tablet:IProducto={
+    desc:'Ipad Air',
+    precio:2583
+}
+
+function calcularIVA(productos:IProducto[]):number{
+    let total=0;
+    productos.forEach((producto)=>{
+            total+=producto.precio
+    })
+    return total*0.16
+}
+
+const articulos1=[telefono, tablet];
+const iva1=calcularIVA(articulos1)
+console.log(`IVA: ${iva1}`)
+
+export function calcularIVA2(productos:IProducto[]):[number,number]{
+    let total=0;
+    productos.forEach(({precio})=>{
+        total+=precio;
+    })
+    return[total,total*0.16];
+}
+
+const artisculos2=[telefono, tablet];
+const [total, iva]=calcularIVA2(artisculos2);
+console.log(`Total: ${total}`);
+console.log(`IVA ${iva}`);
---
+++ src/temasTS/06-Importar-exportar.ts
@@ -0,0 +1,22 @@
+
+
+import { calcularIVA2,IProducto } from "./05-desestructuracior-funciones";
+
+const carrito:IProducto[]=[
+    {
+        desc:'Telefono1',
+        precio:899
+    },
+    {
+        desc:'Telefono2',
+        precio:400
+    },
+    {
+        desc:'Telefono3',
+        precio:798
+    }
+]
+
+const [total,iva]=calcularIVA2(carrito);
+console.log(`Total: ${total}`);
+console.log(`IVA: ${iva}`);
